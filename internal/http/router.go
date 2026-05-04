f340f5c66958a56b297bc393b3d45d3eaf7b0578
amirnak <amirnakgamer@gmail.com>
2026-05-04 12:35:21 +0300

    Add: empty routes for all paths

--- internal/http/router.go
+++ internal/http/router.go
@@ -5,7 +5,14 @@ import "net/http"
 func NewRouter(h *Handler) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/health", h.Health)
+	mux.HandleFunc("GET /health", h.Health)
+
+	mux.HandleFunc("POST /subscriptions", h.CreateSubscription)
+	mux.HandleFunc("GET /subscriptions", h.ListSubscriptions)
+	mux.HandleFunc("GET /subscriptions/{id}", h.GetSubscriptuion)
+	mux.HandleFunc("GET /subscriptions/total-cost", h.GetTotalCost)
+	mux.HandleFunc("PUT /subscriptions/{id}", h.PutSubscriprions)
+	mux.HandleFunc("DELETE /subscriptions/{id}", h.DeleteSubsctiptions)
 
 	return CORSMiddleware(mux)
 }
---
+++ internal/http/subsriptions.go
@@ -0,0 +1,15 @@
+package http
+
+import "net/http"
+
+func (h *Handler) CreateSubscription(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) GetSubscriptuion(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) ListSubscriptions(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) PutSubscriprions(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) DeleteSubsctiptions(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) GetTotalCost(w http.ResponseWriter, r *http.Request) {}
